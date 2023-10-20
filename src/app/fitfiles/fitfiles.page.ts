cf9438fa90c7a0cff63e2b496a8a2516a1c38ef2
john <jkbaima@gmail.com>
2023-10-20 14:13:10 -0500

    Select file to upload to firebase

--- src/app/fitfiles/fitfiles.page.ts
+++ src/app/fitfiles/fitfiles.page.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-fitfiles',
-  templateUrl: './fitfiles.page.html',
-  styleUrls: ['./fitfiles.page.scss'],
+  selector: "app-fitfiles",
+  templateUrl: "./fitfiles.page.html",
+  styleUrls: ["./fitfiles.page.scss"],
 })
 export class FitfilesPage implements OnInit {
+  // Single File Upload
+  private file: File;
 
-  constructor() { }
+  // Multiple File Upload
+  private fileOne: File;
+  private fileTwo: File;
+  private fileThree: File;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  // Single File Upload
+  onFileChange(fileChangeEvent) {
+    this.file = fileChangeEvent.target.files[0];
   }
 
+  async submitForm() {
+    let formData = new FormData();
+    formData.append("photo", this.file, this.file.name);
+
+    const serverUrl = "http://localhost:3000/upload";
+    const nestServerUrl = "http://localhost:3000/photos/upload";
+
+    this.http.post(serverUrl, formData).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  // Multiple File Upload
+  onFileOneChange(fileChangeEvent) {
+    this.fileOne = fileChangeEvent.target.files[0];
+  }
+
+  onFileTwoChange(fileChangeEvent) {
+    this.fileTwo = fileChangeEvent.target.files[0];
+  }
+
+  onFileThreeChange(fileChangeEvent) {
+    this.fileThree = fileChangeEvent.target.files[0];
+  }
+
+  async submitMultipleForm() {
+    let formData = new FormData();
+    formData.append("photos[]", this.fileOne, this.fileOne.name);
+    formData.append("photos[]", this.fileTwo, this.fileTwo.name);
+    formData.append("photos[]", this.fileThree, this.fileOne.name);
+
+    const serverUrl = "http://localhost:3000/uploads";
+    const nestServerUrl = "http://localhost:3000/photos/uploads";
+
+    this.http.post(serverUrl, formData).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  ngOnInit() {}
 }
